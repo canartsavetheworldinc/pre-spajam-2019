08c83232222eacaea3e1bd5d51d6ef1f7026534b
Moosan <washer2016@outlook.jp>
2019-05-01 22:36:02 +0900

    ChangeHome

--- components/Home.js
+++ components/Home.js
@@ -3,14 +3,16 @@ import { Text, View } from "react-native"
 import { Actions } from "react-native-router-flux"
 import Button from "./Button"
 export default class extends React.Component {
+	handlePress = () =>  {
+		Actions.color()
+	}
 	render() {
 		return (
 			<View>
-				<Text>Hello</Text>
-				<Button onPress = {Actions.color} name = "春" spring/>
-				<Button onPress = {Actions.color} name = "夏" sammer/>
-				<Button onPress = {Actions.color} name = "秋" fall/>
-				<Button onPress = {Actions.color} name = "冬" spring/>
+				<Button onPress = {this.handlePress} name = "春" spring/>
+				<Button onPress = {this.handlePress} name = "夏" summer/>
+				<Button onPress = {this.handlePress} name = "秋" fall/>
+				<Button onPress = {this.handlePress} name = "冬" winter/>
 			</View>
 		)
 	}
