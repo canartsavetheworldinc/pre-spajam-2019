4b7d90fd6af82f9c7eab366fb9b3d0b69764e97a
Moosan <washer2016@outlook.jp>
2019-05-01 20:54:08 +0900

    AddButton

--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,16 @@
 import React from "react"
 import { Text, View } from "react-native"
 import { Actions } from "react-native-router-flux"
-
+import Button from "./Button"
 export default class extends React.Component {
 	render() {
 		return (
 			<View>
-				<Text onPress={ Actions.color }>色選択へ</Text>
+				<Text>Hello</Text>
+				<Button onPress = {Actions.color} name = "春" spring/>
+				<Button onPress = {Actions.color} name = "夏" sammer/>
+				<Button onPress = {Actions.color} name = "秋" fall/>
+				<Button onPress = {Actions.color} name = "冬" spring/>
 			</View>
 		)
 	}
