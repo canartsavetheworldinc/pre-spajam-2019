f790c06fb3570754864c471e8880bf82650f3285
tomon9086 <tomo.mouse.9086@gmail.com>
2019-05-02 14:24:54 +0900

    colorstack

--- components/Color.js
+++ components/Color.js
@@ -1,16 +1,21 @@
 import React from "react"
+import { bindActionCreators } from "redux"
+import { connect } from "react-redux"
 import { StyleSheet, Text, View } from "react-native"
 
 import ColorWheel from "./ColorWheel"
 import suggest from "../functions/suggest"
 import ColorStackPanel from "./ColorStackPanel"
-export default class extends React.Component {
+import * as colorActions from "../actions"
+
+class Color extends React.Component {
 	render() {
+		const lastSelected = this.props.selected.slice(-1)[0]
 		return (
 			<View style={ styles.container }>
-				<ColorStackPanel order="asc" />
-				<ColorWheel colors={ suggest() }></ColorWheel>
-				<ColorStackPanel order="desc" />
+				<ColorStackPanel colors={ this.props.selected } order="asc" />
+				<ColorWheel colors={ suggest(lastSelected) }></ColorWheel>
+				<ColorStackPanel colors={ this.props.selected } order="desc" />
 			</View>
 		)
 	}
@@ -22,3 +27,12 @@ const styles = StyleSheet.create({
 		justifyContent: "space-between"
 	}
 })
+
+export default connect(
+	state => ({
+		selected: state.color.selected
+	}),
+	dispatch => ({
+		colorActions: bindActionCreators(colorActions, dispatch)
+	})
+)(Color)
